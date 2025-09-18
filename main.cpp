badee903628d0ca7868ec982ab4c3ecfc24053f5
崔东旭 <cuidongxu2020@outlook.com>
2025-09-18 16:24:49 +0800

    三个相机，修改自定义外参

--- main.cpp
+++ main.cpp
@@ -20,19 +20,24 @@ int main() try {
     XInitThreads(); // 让 X11 支持多线程
     // devices();
     Eigen::Matrix4f T1 = Eigen::Matrix4f::Identity(); //四元数(w, x, y, z)表示任意旋转。融合点云时需要将点云旋转，与相机回正方向相反
-    // 旋转轴（ux，uy，uz）是单位向量（0，0，1）右手法则 45° x=ux⋅sin(θ/2),y=uy⋅sin(θ/2),z=uz⋅sin(θ/2),w=cos(θ/2)。
-    T1.block<3, 3>(0, 0) = Eigen::Quaternionf(0.92388, 0.0f, 0.0f, 0.38268f).toRotationMatrix();
-    T1.block<3, 1>(0, 3) = Eigen::Vector3f(-0.05, -0.05, 0); // 平移向量t
+    // 旋转轴（ux，uy，uz）是单位向量（0，0，1）右手法则 90° x=ux⋅sin(θ/2),y=uy⋅sin(θ/2),z=uz⋅sin(θ/2),w=cos(θ/2)。
+    T1.block<3, 3>(0, 0) = Eigen::Quaternionf(0.7071, 0.0f, 0.0f, 0.7071f).toRotationMatrix();
+    T1.block<3, 1>(0, 3) = Eigen::Vector3f(0, -0.05, 0); // 平移向量t
     Eigen::Matrix4f T2 = Eigen::Matrix4f::Identity();
-    T2.block<3, 3>(0, 0) = Eigen::Quaternionf(0.92388, 0.0f, 0.0f, -0.38268f).toRotationMatrix(); //45°
-    T2.block<3, 1>(0, 3) = Eigen::Vector3f(-0.05, 0.05, 0);
+    T2.block<3, 3>(0, 0) = Eigen::Quaternionf(1.f, 0.0f, 0.0f, 0.0f).toRotationMatrix(); //0°
+    T2.block<3, 1>(0, 3) = Eigen::Vector3f(-0.05, 0, 0);
+    Eigen::Matrix4f T3 = Eigen::Matrix4f::Identity();
+    T2.block<3, 3>(0, 0) = Eigen::Quaternionf(0.7071, 0.0f, 0.0f, -0.7071f).toRotationMatrix(); //-90°
+    T2.block<3, 1>(0, 3) = Eigen::Vector3f(0, 0.05, 0);
     // 外参
     camera_extrinsic extrinsic1("239722073505", 640, 480, 90, T1),
-            extrinsic2("239722072145", 640, 480, 90, T2);
+            extrinsic2("239722072145", 640, 480, 90, T2),
+            extrinsic3("239722073898", 640, 480, 90,T3);
 
     multi_RGBD multi_rgbd;
     multi_rgbd.addCamera(extrinsic1);
     multi_rgbd.addCamera(extrinsic2);
+    multi_rgbd.addCamera(extrinsic3);
     pcl::PointCloud<PolarPoint>::Ptr world_cloud;
 
     std::this_thread::sleep_for(std::chrono::seconds(1)); //等待硬件初始化
